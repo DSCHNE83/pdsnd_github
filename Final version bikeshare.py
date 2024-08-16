e2c9f3cfe3f0691d768a092e2e89bd82b6162053
root <root@localhost>
2024-08-16 11:49:03 +0000

    Add earliest used start station in bikeshare.py

--- Final version bikeshare.py
+++ Final version bikeshare.py
@@ -130,6 +130,10 @@ def station_stats(df):
     common_start_station = df['Start Station'].mode()[0]
     print("The most commonly used start station:" ,common_start_station)
 
+    # TO DO: display earliest used start station
+    earliest_start_station = df['Start Station'].min
+    print("The most commonly used start station:" ,common_start_station)
+
     # TO DO: display most commonly used end station
     common_end_station = df['End Station'].mode()[0]
     print("The most commonly used end station:" ,common_end_station)
